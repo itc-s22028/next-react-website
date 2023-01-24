97acac445e14d0ef0602c3785e7e062a0c868f1b
itc-s22028 <s22028@std.it-college.ac.jp>
2023-01-24 08:27:28 +0000

    React

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
@@ -6,9 +7,17 @@ import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-colum
 import Image from 'next/image'
 import eyecatch from 'images/about.jpg'
 
-export default function About() {
+export default function About () {
   return (
     <Container>
+      <Meta
+        pageTitle='アバウト'
+        pageDesc='About development activiites'
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+
       <Hero title='About' subtitle='About development activities' />
 
       <figure>
@@ -19,28 +28,34 @@ export default function About() {
           sizes='(min-width: 1152px) 1152px, 100vw'
           priority
           placeholder='blur'
-         />
+        />
       </figure>
 
       <TwoColumn>
         <TwoColumnMain>
           <PostBody>
+            <p>                                                                                                                 Cubeが得意とする分野はモノづくりです。３次元から２次元の造形、プログラミングやデザインなど、
+              さまざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
+            </p>
+            <h2>モノづくりで目指していること</h2>
             <p>
-              Cubeが得意とする分野はモノづくりです。3次元から2次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによっ                  て社会や環境と結びつけるクリエイティブを提案し続けています。
+              モノづくりではデータの解析からクリエイティブまで幅広いことを担当しています。新しいことを取り入
+              れながら、ユーザーにマッチした提案を実現するのが目標です。たくさんの開発・提供が数多くありますが、
+              特にそこを磨く作業に力を入れています。
             </p>
             <p>
-              <h2>モノづくりで目指していること</h2>
-            </p>
-            <p>
-              単純に形にするだけでなく、作る過程や、なぜそのようにしたのかを大事にしながらものづくりをしています。毎回課題解決テーマをもって「モノ              」と向き合い制作をし、フィードバックしてもらうことで自分の中にあるモヤモヤを言葉にして「問い」への答えを出しています。
+              単純に形にするだけではなく、作る過程や、なぜそのようにしたのかを大事にしながらものづくりをしてい
+              ます。毎回課題解決テーマをもって「モノ」と向き合い制作をし、フィードバックしてもらうことで自分
+              の中にあるモヤモヤを言葉にして「問い」への答えを出しています。
             </p>
             <h3>新しいことへのチャレンジ</h3>
             <p>
-              今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにして              います。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
+              今までと違うものを作ることで愛着がわいてきます。そこで興味を持ったことは小さなことでもいいから
+              取り入れて、いいものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづ
+              くりは、これからも続けていきたいです。
             </p>
           </PostBody>
         </TwoColumnMain>
-
         <TwoColumnSidebar>
           <Contact />
         </TwoColumnSidebar>
@@ -48,3 +63,4 @@ export default function About() {
     </Container>
   )
 }
+
