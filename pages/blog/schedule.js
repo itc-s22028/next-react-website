c8827e8b7e7575661ce6790e88dcf29a99431e03
itc-s22028 <s22028@std.it-college.ac.jp>
2023-02-08 05:37:01 +0000

    React

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,5 +1,19 @@
 import { getPostBySlug } from 'lib/api'
+
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
+
 import Container from 'components/container'
+import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
+
+import ConvertBody from 'components/convert-body'
+import PostCategories from 'components/post-categories'
+import Image from 'next/image'
+//import { getPlaiceholder } from 'plaiceholder'
+
+import { eyecatchLocal } from 'lib/constants'
 
 export default function Schedule({
   title,
@@ -7,26 +21,70 @@ export default function Schedule({
   content,
   eyecatch,
   categories,
+  description,
 }) {
   return (
     <Container>
-     <h1>{title}</h1>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      <article>
+        <PostHeader title={title} subtitle='Blog Article' publish={publish} />
+
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=''
+            layout='responsive'
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes='(min-width: 1152px) 1152px, 100vw'
+            priority
+    /*
+            placeholder='blur'
+            blurDateURL={eyecatch.blurDateURL}
+            */
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody><ConvertBody contentHTML={content} /></PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
     </Container>
   )
 }
 
-export async function getStaticProps() {
-  const slug = 'schedule'
+export async function getStaticProps () {
+  const slug = 'micro'
 
   const post = await getPostBySlug(slug)
 
+  const description = extractText(post.content)
+
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  /*
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDateURL = base64
+  */
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
+      description: description,
     },
   }
 }
