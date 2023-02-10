af779de4704415e00125e68aa8ea9e6b2ed6e666
itc-s22028 <s22028@std.it-college.ac.jp>
2023-02-10 01:11:43 +0000

    269p

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,12 +1,23 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 
-export default function Blog() {
+export default function Blog () {
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
       <Hero title='Blog' subtitle='Recent Posts' />
     </Container>
   )
+}
+
+export async function getStaticProps () {
+  const posts = await getAllPosts()
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
